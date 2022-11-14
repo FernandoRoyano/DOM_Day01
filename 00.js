fcee7d1788e21cb31bb7f92811bc5fda3a77b88f
FernandoRoyano <fernandoroyano.dev@gmail.com>
2022-11-14 21:24:56 +0100

    final de clase

---
+++ 00.js
@@ -0,0 +1,20 @@
+const element1 = document.querySelector(".element1");
+element1.style.color = "red";
+
+const element2 = document.querySelector(".element2");
+element2.style.color = "blue";
+
+const element3 = document.querySelector(".element3");
+element3.style.color = "green";
+
+const element4 = document.querySelector(".element4");
+element4.style.color = "purple";
+
+const element5 = document.querySelector(".element5");
+element5.style.color = "orange";
+
+
+const colorButton = document.querySelector ('.changeColor')
+colorButton.onclick = () => {
+    if 
+}
